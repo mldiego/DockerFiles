c5e9afb6eda44eb26dbfb09add513b32817939fb
mldiego <diego.manzanas.lopez@vanderbilt.edu>
2023-04-24 12:40:14 -0500

    New nnv docker images

---
+++ NNV/CodeOcean/run_cav23_re.m
@@ -0,0 +1,27 @@
+% install and add paths
+
+addpath(genpath('/deps'));
+
+cd /code/nnv;
+
+run install.m;
+
+matlabshared.supportpkg.setSupportPackageRoot('/usr/local/MATLAB/R2022b');
+addpath(genpath('/usr/local/MATLAB'));
+
+% check if on codeocean/docker, error out if not
+try
+    if ~is_codeocean() % test codeocean detection
+        'ERROR: run_codeocean.m should only be executed on the codeocean platform'
+        return;
+    end
+catch
+    'ERROR: path likely not set, run install.m'
+end
+
+% default output path for path_results, ensure logs subdirectory there
+mkdir('/results/logs/')
+
+cd /code/nnv/examples/NNV2.0/Submission/CAV2023/
+
+run_cav23 % run a subset of all results
